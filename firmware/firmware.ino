c07dfd9d8ed54cf01c47928c6e5553a33ae88ac3
Tom <t.d.archer@gmail.com>
2024-10-22 12:04:03 +0100

    serial comms to BC660

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,20 +1,26 @@
 //#include <SoftwareSerial.h>
 
+#define GSM_RESET D0
+#define GSM_BOOT D1
+#define CO2_PWR  D2     // the number of the pushbutton pin
+#define GSM_PWR  D3      // the number of the LED pin
 
-const int CO2_PWR = 2;     // the number of the pushbutton pin
-const int GSM_PWR =  3;      // the number of the LED pin
 
 //SoftwareSerial mySerial(2,3); //RX,TX
 
 void setup(){
-  Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial.begin(115200);
+  Serial1.begin(115200);
   pinMode(GSM_PWR, OUTPUT);
   pinMode(CO2_PWR, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);  
-  digitalWrite(GSM_PWR, LOW);
-  digitalWrite(CO2_PWR, LOW);
-  digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(GSM_PWR, HIGH); // high is off
+  digitalWrite(CO2_PWR, HIGH); // high is off
+  digitalWrite(LED_BUILTIN,HIGH); //low is on 
+  digitalWrite(GSM_BOOT, HIGH); //pullup to HIGH
+  digitalWrite(GSM_RESET, HIGH); //pullup to HIGH
+  delay(1000);
+
   /*Serial.begin(115200);
   mySerial.begin(115200);
   delay(1000);
@@ -42,7 +48,29 @@ void setup(){
   b="";*/
 }
 
+void GSM(){
+  digitalWrite(GSM_PWR, LOW); // low is on
+  digitalWrite(CO2_PWR, HIGH); // high is off
+  digitalWrite(LED_BUILTIN,LOW); //low is on 
+  digitalWrite(GSM_BOOT, HIGH); //pullup to HIGH
+  digitalWrite(GSM_RESET, HIGH); //pullup to HIGH
+
+  Serial1.println("AT");
+  int i =0;
+  while( !Serial1.available() ) {
+    delay(100);
+    i++;
+    if (i>1000){
+      break;
+    }
+  }
+  Serial.write(Serial1.read());   
+}
+
+
 void loop(){
+  
+  //Serial.println("Hello");
   /*digitalWrite(GSM_PWR, LOW);
   digitalWrite(CO2_PWR, LOW);
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
@@ -53,6 +81,9 @@ void loop(){
   delay(5000);  */
   if (Serial.available()) {      // If anything comes in Serial (USB),
     Serial1.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
+    digitalWrite(LED_BUILTIN,HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN,LOW);
   }
 
   if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
